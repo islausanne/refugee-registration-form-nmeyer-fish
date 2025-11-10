0b2825cf9cb91e18c7efd1d46518a150717ebc57
nmeyer-fish <nmeyer@islstudent.ch>
2025-11-10 12:32:38 +0100

    Begging

---
+++ Refugee Registration Form.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Welcome to the Homepage!"
+
+@app.route('/about')
+def about():
+    return "This is the About Page."
+
+@app.route('/contact')
+def contact():
+    return "Contact us at: info@example.com."
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
